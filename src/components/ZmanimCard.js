78930e8d40584c574883d6f3952fb78420adbcbb
tzivi05-gif <tzivi05@gmail.com>
2026-01-15 21:35:51 -0500

    Fix zmanim field names to match Hebcal API

--- src/components/ZmanimCard.js
+++ src/components/ZmanimCard.js
@@ -66,19 +66,20 @@ function ZmanimCard() {
             <p className="timezone">Timezone: {zmanim.timezone}</p>
 
             <table className="zmanim-table">
-              <tbody>
-                <tr><td>Alot Hashachar</td><td>{formatTime(zmanim.times.alotHaShachar)}</td></tr>
-                <tr><td>Sunrise</td><td>{formatTime(zmanim.times.sunrise)}</td></tr>
-                <tr><td>Latest Shema</td><td>{formatTime(zmanim.times.sofZmanShma)}</td></tr>
-                <tr><td>Latest Tefillah</td><td>{formatTime(zmanim.times.sofZmanTfila)}</td></tr>
-                <tr><td>Chatzot</td><td>{formatTime(zmanim.times.chatzot)}</td></tr>
-                <tr><td>Mincha Gedola</td><td>{formatTime(zmanim.times.minchaGedola)}</td></tr>
-                <tr><td>Plag HaMincha</td><td>{formatTime(zmanim.times.plagHaMincha)}</td></tr>
-                <tr><td>Sunset</td><td>{formatTime(zmanim.times.sunset)}</td></tr>
-                <tr><td>Nightfall (Tzeit)</td><td>{formatTime(zmanim.times.tzeit)}</td></tr>
-              </tbody>
-            </table>
-          </>
+  <tbody>
+    <tr><td>Alot Hashachar</td><td>{formatTime(zmanim.times.alot)}</td></tr>
+    <tr><td>Sunrise</td><td>{formatTime(zmanim.times.sunrise)}</td></tr>
+    <tr><td>Latest Shema</td><td>{formatTime(zmanim.times.sof_zman_shma)}</td></tr>
+    <tr><td>Latest Tefillah</td><td>{formatTime(zmanim.times.sof_zman_tfila)}</td></tr>
+    <tr><td>Chatzot</td><td>{formatTime(zmanim.times.chatzot)}</td></tr>
+    <tr><td>Mincha Gedola</td><td>{formatTime(zmanim.times.mincha_gedola)}</td></tr>
+    <tr><td>Plag HaMincha</td><td>{formatTime(zmanim.times.plag_hamincha)}</td></tr>
+    <tr><td>Sunset</td><td>{formatTime(zmanim.times.sunset)}</td></tr>
+    <tr><td>Nightfall (Tzeit)</td><td>{formatTime(zmanim.times.tzeit)}</td></tr>
+  </tbody>
+</table>
+
+                      </>
         )}
       </div>
     </div>
